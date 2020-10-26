b65cd2ec3e9aff00c04636cbb66bf26d40811f53
Azwad Rafique <62928702+AzwadRafique@users.noreply.github.com>
2020-10-26 10:51:05 +0600

    Update Pizza Confusion.py

--- Pizza Confusion.py
+++ Pizza Confusion.py
@@ -10,23 +10,25 @@ for x in range(number_of_times):
 swapped = True
 for y in range(len(scores)):
     swapped = False
-    for placement in range(len(scores) - 1):
-        if scores[placement] < scores[placement + 1]:
+    for position in range(len(scores) - 1):
+        if scores[position] < scores[position + 1]:
             swapped = True
-            temporary_score = scores[placement + 1]
-            scores[placement + 1] = scores[placement]
-            scores[placement] = temporary_score
-            temporary_names = names[placement + 1]
-            names[placement + 1] = names[placement]
-            names[placement] = temporary_names
+            temporary_score = scores[position + 1]
+            scores[position + 1] = scores[position]
+            scores[position] = temporary_score
+            temporary_names = names[position + 1]
+            names[position + 1] = names[position]
+            names[position] = temporary_names
+
+        if not swapped:
+            break
+
+for y in range(len(names)):
+    for placement in range(len(names) - 1):
         if scores[placement] == scores[placement + 1]:
             if names[placement] > names[placement + 1]:
                 temporary_alphabet = names[placement + 1]
                 names[placement + 1] = names[placement]
                 names[placement] = temporary_alphabet
-
-    if not swapped:
-        break
-
 
 print(names[0])
